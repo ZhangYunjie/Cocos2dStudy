999d38f57cecf2612f728de621322464620cc6a0
zhang_yunjie <achillessatan@gmail.com>
2014-03-07 17:43:38 +0900

    create the project

---
+++ Cocos2dStudy/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Cocos2dStudyAppController.h
+//  Cocos2dStudy
+//
+//  Created by zhang yunjie on 2014/03/07.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Cocos2dStudy/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  Cocos2dStudyAppController.h
+//  Cocos2dStudy
+//
+//  Created by zhang yunjie on 2014/03/07.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
